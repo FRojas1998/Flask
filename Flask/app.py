e3d2ba656aa5225d59b0112271c66dee6b8470b2
Felipe Rojas <feliperojasgonzalez381@gmail.com>
2024-07-28 17:28:57 -0400

    .

---
+++ Flask/app.py
@@ -0,0 +1,20 @@
+from flask import Flask , jsonify
+import psutil
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_metrics():
+    cpu_percent = psutil.cpu_percent()
+    ram_percent = psutil.virtual_memory().percent
+
+    metrics = {
+        'CPU del Sistema en (%)': cpu_percent,
+        'RAM del Sistema en (%)': ram_percent
+    }
+
+    return jsonify(metrics)
+
+if __name__ == '__main__':
+    app.run(debug=True)
