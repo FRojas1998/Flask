33870eccfabb6581d27ab2f1eb2c20b79c2bdacd
Felipe Rojas <feliperojasgonzalez381@gmail.com>
2024-07-30 14:02:23 -0400

    .

--- Flask/app.py
+++ Flask/app.py
@@ -17,4 +17,9 @@ def get_metrics():
     return jsonify(metrics)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
+#if __name__ == '__main__':
+    #app.run(debug=True)
+#    app.run(debug=True)
